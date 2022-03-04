9fc9551ac8006f158b8bde930c6c3a67cd9ccf7c
lam29121996 <lam29121996@gmail.com>
2022-03-04 18:29:22 +0800

    2022-03-04

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
+## Build
+GOOS=windows GOARCH=amd64 go build -o helmet_detection_server.exe main.go
+
 ## Default config
 ```sh
 {
     "port": 8080,
-    "timeout(ms)": 5000,
-    "images_file_path": "./images"
+    "timeout(ms)": 3500,
+    "images_hiu_ming_folder_path": "./hiuMingImages",
+    "images_hiu_kwong_folder_path": "./hiuKwongImages",
+    "record_from": "07:30",
+    "record_to": "18:30"
 }
 ```
 
 ## Request & Expected output
 ```http
-GET /helmetDetectionResult
+GET /helmetDetectionResult?id=1
 ```
 ```sh
 {
--- main.go
+++ main.go
@@ -1,93 +1,139 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
 type config struct {
-	Port           int    `json:"port"`
-	Timeoutms      int    `json:"timeout(ms)"`
-	ImagesFilePath string `json:"images_file_path"`
+	Port                     int    `json:"port"`
+	Timeoutms                int    `json:"timeout(ms)"`
+	ImagesHiuMingFolderPath  string `json:"images_hiu_ming_folder_path"`
+	ImagesHiuKwongFolderPath string `json:"images_hiu_kwong_folder_path"`
+	RecordFrom               string `json:"record_from"`
+	recordFromHour           int
+	recordFromMinute         int
+	RecordTo                 string `json:"record_to"`
+	recordToHour             int
+	recordToMinute           int
 }
 
 var (
 	cfg config
 
-	resCh chan string
+	imagesHiuMingLastUpdateAt  time.Time
+	imagesHiuKwongLastUpdateAt time.Time
 )
+
+func toFileName(t time.Time, id string) string {
+	str := t.Format(time.RFC3339)
+	str = str[:19]
+	str = strings.ReplaceAll(str, "T", "_")
+	str = strings.ReplaceAll(str, ":", "_")
+
+	if id == "1" {
+		return fmt.Sprintf("%s/%s.jpg", cfg.ImagesHiuMingFolderPath, str)
+	}
+
+	if id == "2" {
+		return fmt.Sprintf("%s/%s.jpg", cfg.ImagesHiuKwongFolderPath, str)
+	}
+
+	return ""
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World! This is indexHandler of helmet_detection_server!"))
 }
 
-func postHelmetDetectionResultHandler(w http.ResponseWriter, r *http.Request) {
-	// log.Println(r.URL)
+func createImage(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
 
-	// Let see how camera send the photo to me, file / file path of the photo?
-	// no helmet detected -> the event is triggered -> send photo
-
-	resCh <- r.RequestURI
-}
-
-func createImage(w http.ResponseWriter, request *http.Request) {
-	err := request.ParseMultipartForm(32 << 20) // maxMemory 32MB
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+	if now.Hour() < cfg.recordFromHour || (now.Hour() == cfg.recordFromHour && now.Minute() < cfg.recordFromMinute) {
+		log.Println("too early!")
 		return
 	}
-	//Access the photo key - First Approach
-	file, h, err := request.FormFile("photo")
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+	if (now.Hour() == cfg.recordToHour && now.Minute() > cfg.recordToMinute) || now.Hour() >= cfg.recordToHour {
+		log.Println("too late!")
 		return
 	}
 
-	tmpfile, err := os.Create("./images/" + h.Filename)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+	id := r.URL.Query().Get("id")
+
+	switch id {
+	case "1":
+		imagesHiuMingLastUpdateAt = now
+	case "2":
+		imagesHiuKwongLastUpdateAt = now
+	default:
+		log.Println("no ID!")
 	}
 
-	defer tmpfile.Close()
-
-	_, err = io.Copy(tmpfile, file)
+	file, err := os.Create(toFileName(now, id))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		log.Println("os.Create failed!", err)
+	}
+
+	log.Printf("Images from id: %s created!\n", id)
+
+	defer file.Close()
+
+	_, err = io.Copy(file, r.Body)
+	if err != nil {
+		log.Println("io.Copy failed!", err)
 	}
 }
 
 func helmetDetectionResult(w http.ResponseWriter, r *http.Request) {
 	startAt := time.Now()
+	id := r.URL.Query().Get("id")
+
+	if id == "" {
+		w.Write([]byte("ID required!"))
+		return
+	}
 
 	type Response struct {
 		IsHelmetOn bool   `json:"is_helmet_on"`
-		PhotoPath  string `json:"photo_path"`
+		ImagePath  string `json:"image_path"`
 	}
 
 	resp := Response{}
 
-	select {
-	case res := <-resCh:
-		resp = Response{IsHelmetOn: false, PhotoPath: res}
-	case <-time.After(time.Duration(cfg.Timeoutms) * time.Millisecond):
-		resp = Response{IsHelmetOn: true, PhotoPath: ""}
+	time.Sleep(time.Duration(cfg.Timeoutms) * time.Millisecond)
+
+	switch id {
+	case "1":
+		if time.Since(imagesHiuMingLastUpdateAt) <= time.Duration(cfg.Timeoutms)*time.Millisecond {
+			resp = Response{IsHelmetOn: false, ImagePath: toFileName(imagesHiuMingLastUpdateAt, id)}
+		} else {
+			resp = Response{IsHelmetOn: true, ImagePath: ""}
+		}
+	case "2":
+		if time.Since(imagesHiuKwongLastUpdateAt) <= time.Duration(cfg.Timeoutms)*time.Millisecond {
+			resp = Response{IsHelmetOn: false, ImagePath: toFileName(imagesHiuKwongLastUpdateAt, id)}
+		} else {
+			resp = Response{IsHelmetOn: true, ImagePath: ""}
+		}
+	default:
+		w.Write([]byte("invalid ID!"))
+		return
 	}
 
 	b, err := json.Marshal(resp)
 	if err != nil {
 		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -96,6 +142,7 @@ func helmetDetectionResult(w http.ResponseWriter, r *http.Request) {
 	_, err = w.Write(b)
 	if err != nil {
 		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
@@ -104,14 +151,7 @@ func helmetDetectionResult(w http.ResponseWriter, r *http.Request) {
 
 func init() {
 	// Read config.json
-	f, err := os.Open("config.json")
-	if err != nil {
-		log.Panicln("Open config.json failed!", err)
-	}
-
-	defer f.Close()
-
-	b, err := ioutil.ReadAll(f)
+	b, err := ioutil.ReadFile("config.json")
 	if err != nil {
 		log.Panicln("Read config.json failed!", err)
 	}
@@ -121,69 +161,85 @@ func init() {
 		log.Panicln("Unmarshal config.json failed!", err)
 	}
 
-	// Make resCh
-	resCh = make(chan string)
+	strs := strings.Split(cfg.RecordFrom, ":")
+
+	cfg.recordFromHour, err = strconv.Atoi(strs[0])
+	if err != nil {
+		log.Panicln("strconv.Atoi failed!", err)
+	}
+	cfg.recordFromMinute, err = strconv.Atoi(strs[1])
+	if err != nil {
+		log.Panicln("strconv.Atoi failed!", err)
+	}
+
+	strs = strings.Split(cfg.RecordTo, ":")
+
+	cfg.recordToHour, err = strconv.Atoi(strs[0])
+	if err != nil {
+		log.Panicln("strconv.Atoi failed!", err)
+	}
+	cfg.recordToMinute, err = strconv.Atoi(strs[1])
+	if err != nil {
+		log.Panicln("strconv.Atoi failed!", err)
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandler)
-	// r.HandleFunc("/helmetDetectionResult", postHelmetDetectionResultHandler).Methods("POST")
 	r.HandleFunc("/createImage", createImage).Methods("POST")
 	r.HandleFunc("/helmetDetectionResult", helmetDetectionResult).Methods("GET")
-	r.PathPrefix("/images").Handler(http.StripPrefix("/images", http.FileServer(http.Dir(cfg.ImagesFilePath)))).Methods("GET")
+	r.PathPrefix("/hiuMingImages").Handler(http.StripPrefix("/hiuMingImages", http.FileServer(http.Dir(cfg.ImagesHiuMingFolderPath)))).Methods("GET")
+	r.PathPrefix("/hiuKwongImages").Handler(http.StripPrefix("/hiuKwongImages", http.FileServer(http.Dir(cfg.ImagesHiuKwongFolderPath)))).Methods("GET")
 
 	// Client routine
-	go func() {
-		time.Sleep(3 * time.Second)
+	/*
+		go func() {
+			call := func(urlPath, method string) error {
+				client := &http.Client{
+					Timeout: time.Second * 10,
+				}
 
-		call := func(urlPath, method string) error {
-			client := &http.Client{
-				Timeout: time.Second * 10,
+				img, err := os.Open("five000000.png")
+				if err != nil {
+					log.Println(err)
+				}
+
+				defer img.Close()
+
+				req, err := http.NewRequest(method, urlPath, img)
+				if err != nil {
+					return err
+				}
+
+				req.Header.Set("Content-Type", "image")
+
+				rsp, _ := client.Do(req)
+				if rsp.StatusCode != http.StatusOK {
+					log.Printf("Request failed with response code: %d", rsp.StatusCode)
+				}
+
+				return nil
 			}
 
-			// New multipart writer.
-			body := &bytes.Buffer{}
-			writer := multipart.NewWriter(body)
+			reader := bufio.NewReader(os.Stdin)
 
-			fw, err := writer.CreateFormFile("photo", "five000000.png")
-			if err != nil {
-				return err
+			for {
+				reader.ReadString('\n')
+
+				for i := 0; i < 3; i++ {
+					err := call("http://localhost:8080/createImage", "POST")
+					log.Println("client called!")
+					if err != nil {
+						log.Println(err)
+					}
+					time.Sleep(1 * time.Second)
+				}
 			}
+		}()
+	*/
 
-			file, err := os.Open("five000000.png")
-			if err != nil {
-				return err
-			}
-
-			_, err = io.Copy(fw, file)
-			if err != nil {
-				return err
-			}
-
-			writer.Close()
-
-			req, err := http.NewRequest(method, urlPath, bytes.NewReader(body.Bytes()))
-			if err != nil {
-				return err
-			}
-
-			req.Header.Set("Content-Type", writer.FormDataContentType())
-
-			rsp, _ := client.Do(req)
-			if rsp.StatusCode != http.StatusOK {
-				log.Printf("Request failed with response code: %d", rsp.StatusCode)
-			}
-
-			return nil
-		}
-
-		err := call("http://localhost:8080/createImage", "POST")
-		if err != nil {
-			log.Println(err)
-		}
-		log.Println("client called!")
-	}()
-
+	log.Printf("This server only save no helmet photo from %s to %s.\n", cfg.RecordFrom, cfg.RecordTo)
+	log.Printf("Listen & Serve at port: %d", cfg.Port)
 	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), r)
 }
